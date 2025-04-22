2a231154eeb5b73a8c9f654b90dae6243a581375
Ornsiri <k.ornsiripim@gmail.com>
2025-04-22 16:27:22 +0700

    Practice with Spring data rest

--- crud/src/main/java/com/ornsiri/springboot/demo/crud/dao/EmployeeRepository.java
+++ crud/src/main/java/com/ornsiri/springboot/demo/crud/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.ornsiri.springboot.demo.crud.dao;
 
 import com.ornsiri.springboot.demo.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // No need to define any methods here, JpaRepository provides all the necessary CRUD operations
     // You can add custom query methods if needed
--- crud/src/main/java/com/ornsiri/springboot/demo/crud/service/EmployeeService.java
+++ crud/src/main/java/com/ornsiri/springboot/demo/crud/service/EmployeeService.java
@@ -1,13 +1,13 @@
-package com.ornsiri.springboot.demo.crud.service;
-
-import com.ornsiri.springboot.demo.crud.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> findAll();
-    Employee findById(int id);
-    Employee save(Employee employee);
-    void deleteById(int id);
-
-}
+//package com.ornsiri.springboot.demo.crud.service;
+//
+//import com.ornsiri.springboot.demo.crud.entity.Employee;
+//
+//import java.util.List;
+//
+//public interface EmployeeService {
+//    List<Employee> findAll();
+//    Employee findById(int id);
+//    Employee save(Employee employee);
+//    void deleteById(int id);
+//
+//}
